a03477d32a8cd90dac0d92f856897a0930b16bef
zachdotscott <133394075+zachdotscott@users.noreply.github.com>
2024-03-31 13:50:48 -0400

    Update myscripts.js

--- myscripts.js
+++ myscripts.js
@@ -2,4 +2,15 @@ function task1(){
     alert('XSS');
 }
 
-// task1();
+function listCookies() {
+    var theCookies = document.cookie.split(';');
+    var aString = '';
+    for (var i = 1 ; i <= theCookies.length; i++) {
+        aString += i + ' ' + theCookies[i-1] + "\n";
+    }
+    return aString;
+}
+
+function task2(){
+    alert(listCookies());
+}
